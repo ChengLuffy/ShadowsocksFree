8efcf21d8ae8350e84b2ec4f1e0991c115cf440e
ChengLuffy <chengluffy@hotmail.com>
2016-05-03 11:27:06 +0800

    Initial Commit

---
+++ ShadowsocksFree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowsocksFree
+//
+//  Created by 成璐飞 on 16/5/3.
+//  Copyright © 2016年 成璐飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
