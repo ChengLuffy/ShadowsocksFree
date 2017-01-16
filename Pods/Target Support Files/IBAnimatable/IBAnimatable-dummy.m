05dfa18b23c95b9c4a7839b49287d2002c76418f
成璐飞 <chengluffy@hotmail.com>
2017-01-16 22:09:18 +0800

    add gitignore

--- Pods/Target Support Files/IBAnimatable/IBAnimatable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IBAnimatable : NSObject
-@end
-@implementation PodsDummy_IBAnimatable
-@end
--- Pods/Target Support Files/IBAnimatable/IBAnimatable-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double IBAnimatableVersionNumber;
-FOUNDATION_EXPORT const unsigned char IBAnimatableVersionString[];
-
