589790192209dde231b718fc6247acf2be36820b
成殿 <chengluffy@hotmail.com>
2018-09-04 16:07:56 +0800

    update

--- ShadowsocksFree/ViewController/ViewController.swift
+++ ShadowsocksFree/ViewController/ViewController.swift
@@ -73,7 +73,7 @@ class ViewController: UIViewController {
                 tableView.reloadData()
             }
             */
-            tableView.mj_header.beginRefreshing()
+            getData()
         } else {
             tableView.mj_header.beginRefreshing()
         }
